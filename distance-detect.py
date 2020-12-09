25afcb4c902202dae064fbc89808d533dd8ec940
Lourdvic <lourdvic.alcindor@epitech.eu>
2020-12-09 23:42:29 +0100

    Comment added
    Comment was addeed to understand better

--- distance-detect.py
+++ distance-detect.py
@@ -1,33 +1,50 @@
+# import required libs 
+
+# to get the mouse event 
 from tkinter import *
 from PIL import ImageTk, Image
+
+# to compute the scale
 import math
+
+# to draw the lines
 import cv2
 
 
 root = Tk()
 imgpath = "plan\white plan\plan-maison-traditionnelle-maison-gallegos-ebe25052a-775x450.jpeg"
 img = ImageTk.PhotoImage(Image.open(imgpath))
+
+# load the clone image to draw on it
 dimg = cv2.imread(imgpath)
 window_name = "plan image"
+
 panel = Label(root, image=img)
 panel.pack(side="bottom", fill="both", expand="yes")
+
+# initiate the values
 counter = 0
 x0 = 0
 x1 = 0
 y0 = 0
 y1 = 0
 
-
+# function to compute distance between mouse event
 def getDistance(event):
     global counter, x0, y0, x1, y1
+
+    # when we click the counter is incremented to recognize the different phase
+    # 1st click -> get the x0 & y0 coord
     if counter == 0:
         x0 = event.x
         y0 = event.y
         counter += 1
+    # 2nd click get the x1 and y1 coord
     elif counter == 1:
         x1 = event.x
         y1 = event.y
         counter += 1
+    # 3rd click draw the line on the clone image and compute the distance then give the size of the wall
     elif counter == 2:
 
         #get info to draw on image 
@@ -46,6 +63,7 @@ def getDistance(event):
         print(reeldim)
         counter = 0
 
-
+# button 1 get the mouse event info
 panel.bind("<Button-1>", getDistance)
+# run the app till we close it 
 root.mainloop()
